365693ad8c4be27d0a518481f27dd9c9d1fd8326
msKaleb <sjmikel@gmx.es>
2025-08-01 15:02:34 +0200

    test api call to OpenWeatherMap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import WeatherComponent from "@/components/test";
 
+export default function Home() {
+  return <WeatherComponent />;
+}
+
+/*
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -101,3 +107,4 @@ export default function Home() {
     </div>
   );
 }
+*/
